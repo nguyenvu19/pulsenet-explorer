0d15eee8ee6805020dc9a79f08ede765603eff3d
DavidNgo <ngohung2395@gmail.com>
2022-11-11 08:13:51 +0700

    update: nav

--- src/components/MenuBlock/HeaderBlock.js
+++ src/components/MenuBlock/HeaderBlock.js
@@ -6,8 +6,8 @@ import SearchInput from 'components/SearchInput'
 import MenuCollapseIcon from 'widgets/MenuCollapseIcon'
 import config from '../Menu/config'
 import ContentMobile from '../Menu/ContentMobile/ContentMobile'
-import siteConfig from '../../config/site.config'
 import useMatchBreakpoints from 'hooks/useMatchBreakpoints'
+import Account from './Account'
 
 const HeaderBlock = ({ showMenu, toggleMenu }) => {
   const router = useRouter()
@@ -19,75 +19,77 @@ const HeaderBlock = ({ showMenu, toggleMenu }) => {
         <div className="nav-top">
           <div className="nav-top-content container">
             <div className="nav-top-left">
-              <Link href="/">
-                <div className="nav-logo">
-                  <img src="/logo-text.png" alt="" />
-                </div>
+              <Link href="/" className="nav-logo">
+                <img src="/logo-text.png" alt="" />
               </Link>
             </div>
             <div className="nav-top-right">
-              <div className="nav-right-input">
-                <SearchInput />
-              </div>
-              {isMobile && <MenuCollapseIcon className="menu-collapse" showMenu={showMenu} toggleMenu={toggleMenu} />}
+              {isMobile ? (
+                <MenuCollapseIcon className="menu-collapse" showMenu={showMenu} toggleMenu={toggleMenu} />
+              ) : (
+                <div className="nav-search-input">
+                  <SearchInput />
+                </div>
+              )}
             </div>
           </div>
         </div>
         {!isMobile && (
-          <ul className="nav-bottom container">
-            {config.map((entry, index) => {
-              if (entry.items) {
-                return (
-                  <li key={`${entry.label}-${index}`}>
-                    <Link href="/" className="nav-blockchain">
-                      Blockchain
-                      <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                          <path d="M192 384c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L192 306.8l137.4-137.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-160 160C208.4 380.9 200.2 384 192 384z" />
-                        </svg>
-                      </span>
-                    </Link>
-                    <ul className="submenu">
-                      {entry.items.map((subMenu, subIndex) => (
-                        <React.Fragment key={`${subMenu.label}-${subIndex}`}>
-                          <li className="li-acount">
-                            <Link href={subMenu.href} className="navlink-submenu" {...subMenu}>
-                              {subMenu.label}
-                            </Link>
-                          </li>
-                          {subMenu.showBorder && <li className="dropdown-divider" />}
-                        </React.Fragment>
-                      ))}
-                    </ul>
-                  </li>
-                )
-              }
-              return (
-                <li key={`${entry.label}-${index}`}>
-                  <Link href={entry.href} className={router.pathname === entry.href ? 'active' : ''} {...entry}>
-                    {entry.label}
-                  </Link>
-                </li>
-              )
-            })}
-            <li className="nav-l-separator" />
-            <li className="li-last_child li-last_child-block">
-              <Link className="right-destop-logo">
-                <img src="/logo.png" alt="" />
-              </Link>
-              <ul className="submenu">
-                <li className="li-acount">
-                  <Link href="/" className="navlink-submenu">
-                    {siteConfig.appName} Testnet
-                  </Link>
-                </li>
+          <div className="nav-bottom container">
+            <div className="nav-bottom-left">
+              Eth: $1.234.97(<span style={{ color: 'green' }}>+2,03%</span>)
+            </div>
+            <div className="nav-bottom-right">
+              <ul>
+                {config.map((entry, index) => {
+                  if (entry.items) {
+                    return (
+                      <li key={`${entry.label}-${index}`}>
+                        <Link href="/" className="nav-blockchain">
+                          {entry.label}
+                          <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                              <path d="M192 384c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L192 306.8l137.4-137.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-160 160C208.4 380.9 200.2 384 192 384z" />
+                            </svg>
+                          </span>
+                        </Link>
+                        <ul className="submenu fadeIn">
+                          {entry.items.map((subMenu, subIndex) => (
+                            <React.Fragment key={`${subMenu.label}-${subIndex}`}>
+                              <li>
+                                <Link href={subMenu.href} className="navlink-submenu" {...subMenu}>
+                                  {subMenu.label}
+                                </Link>
+                              </li>
+                              {subMenu.showBorder && <li className="dropdown-divider" />}
+                            </React.Fragment>
+                          ))}
+                        </ul>
+                      </li>
+                    )
+                  }
+                  return (
+                    <li key={`${entry.label}-${index}`}>
+                      <Link href={entry.href} className={router.pathname === entry.href ? 'active' : ''} {...entry}>
+                        {entry.label}
+                      </Link>
+                    </li>
+                  )
+                })}
               </ul>
-            </li>
-          </ul>
+              <div className="nav-account">
+                <Account />
+              </div>
+            </div>
+          </div>
         )}
-        {showMenu && <ContentMobile login={false} />}
+        {showMenu && (
+          <>
+            <ContentMobile login={false} links={config} />
+          </>
+        )}
         {isMobile && (
-          <div className="nav-bottom-mobile">
+          <div className="nav-search-input-mobile">
             <SearchInput />
           </div>
         )}
--- src/widgets/Svg/index.js
+++ src/widgets/Svg/index.js
@@ -1,6 +1,8 @@
-export { default as AddIcon } from './Icons/Add';
-export { default as EditIcon } from './Icons/Edit';
-export { default as BackIcon } from './Icons/Back';
-export { default as ExclamationIcon } from './Icons/Exclamation';
+export { default as AddIcon } from './Icons/Add'
+export { default as EditIcon } from './Icons/Edit'
+export { default as BackIcon } from './Icons/Back'
+export { default as UserIcon } from './Icons/User'
+export { default as ExclamationIcon } from './Icons/Exclamation'
+export { default as SearchIcon } from './Icons/Search'
 
-export { default as Svg } from './Svg';
+export { default as Svg } from './Svg'
