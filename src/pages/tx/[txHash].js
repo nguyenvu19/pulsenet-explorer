978cf0868be27c1285c9355d12aebe2ac8cbce32
Tri Le <tri.le@codecomplete.jp>
2022-11-20 22:23:59 +0700

    update api get detail transaction

--- src/pages/tx/[txHash].js
+++ src/pages/tx/[txHash].js
@@ -1,6 +1,6 @@
 import PublicLayoutBlock from 'layouts/PublicLayoutBlock';
 import { getBlockDetail, getListBlocks } from 'services/api/blocks';
-import { getListTransactions } from 'services/api/transactions'
+import { getTransactionDetail } from 'services/api/transactions'
 
 import TxView from 'views/Tx'
 
@@ -17,18 +17,18 @@ export async function getServerSideProps(context) {
         txDetail,
         latestBlock,
     ] = await Promise.all([
-        getListTransactions({ a: txHash }),
+        getTransactionDetail(txHash),
         getListBlocks({ page: 1, page_size: 1 }),
     ]);
 
     let blockDetail = {};
-    if (txDetail?.data?.[0]?.bn) {
-        blockDetail = await getBlockDetail(txDetail?.data?.[0]?.bn)
+    if (txDetail?.data?.bn) {
+        blockDetail = await getBlockDetail(txDetail?.data?.bn)
     }
 
     return {
         props: {
-            txDetail: txDetail?.data?.[0] || {},
+            txDetail: txDetail?.data || {},
             blockDetail: blockDetail?.data?.[0] || {},
             latestBlock: latestBlock?.data?.[0] || {},
         },
